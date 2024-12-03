d853e7d3ea35ff1b051a11b446b79d2a699e9327
dylan-smith <dylan-smith@github.com>
2024-12-03 12:30:29 -0500

    Day 1

--- src/Days/Day01.cs
+++ src/Days/Day01.cs
@@ -5,11 +5,48 @@ public class Day01 : BaseDay
 {
     public override string PartOne(string input)
     {
-        return string.Empty;
+        var left = new List<int>();
+        var right = new List<int>();
+
+        foreach (var line in input.Lines())
+        {
+            left.Add(line.Integers().First());
+            right.Add(line.Integers().Last());
+        }
+
+        left.Sort();
+        right.Sort();
+
+        var totalDistance = 0;
+
+        for (int i = 0; i < left.Count; i++)
+        {
+            totalDistance += Math.Abs(left[i] - right[i]);
+        }
+
+        return totalDistance.ToString();
     }
 
     public override string PartTwo(string input)
     {
-        return string.Empty;
+        var left = new List<int>();
+        var right = new List<int>();
+
+        foreach (var line in input.Lines())
+        {
+            left.Add(line.Integers().First());
+            right.Add(line.Integers().Last());
+        }
+
+        var similarity = 0L;
+
+        foreach (var item in left)
+        {
+            var count = right.Count(x => x == item);
+
+            similarity += item * count;
+        }
+
+        return similarity.ToString();
     }
 }

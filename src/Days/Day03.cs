798de8280eb735f5aac103da230c9899a567bb10
dylan-smith <dylan-smith@github.com>
2024-12-03 12:55:51 -0500

    day 3.1

--- src/Days/Day03.cs
+++ src/Days/Day03.cs
@@ -7,7 +7,34 @@ public class Day03 : BaseDay
 {
     public override string PartOne(string input)
     {
-        return string.Empty;
+        var result = 0L;
+
+        for (var i = 0; i < input.Length - 8; i++)
+        {
+            var foo = input.Substring(i, 4);
+            if (input.Substring(i, 4) == "mul(")
+            {
+                var comma = input.IndexOf(',', i + 4);
+                var endParen = input.IndexOf(')', comma + 1);
+
+                if (comma > 0 && endParen > 0)
+                {
+                    var foo2 = input.Substring(i + 4, comma - (i + 4));
+
+                    if (int.TryParse(input.Substring(i + 4, comma - (i + 4)), out var arg1))
+                    {
+                        var foo3 = input.Substring(comma + 1, endParen - comma - 1);
+
+                        if (int.TryParse(input.Substring(comma + 1, endParen - comma - 1), out var arg2))
+                        {
+                            result += arg1 * arg2;
+                        }
+                    }
+                }
+            }
+        }
+
+        return result.ToString();
     }
 
     public override string PartTwo(string input)

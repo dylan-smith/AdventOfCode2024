f8d3583868dbe36688774f4d1c08ed10e908821c
dylan-smith <dylan-smith@github.com>
2024-12-03 13:15:03 -0500

    day 3 refactor

--- src/Days/Day03.cs
+++ src/Days/Day03.cs
@@ -7,60 +7,43 @@ public class Day03 : BaseDay
 {
     public override string PartOne(string input)
     {
-        var result = 0L;
-
-        for (var i = 0; i < input.Length - 8; i++)
-        {
-            if (input.Substring(i, 4) == "mul(")
-            {
-                var comma = input.IndexOf(',', i + 4);
-                var endParen = input.IndexOf(')', comma + 1);
-
-                if (comma > 0 && endParen > 0)
-                {
-                    if (int.TryParse(input.AsSpan(i + 4, comma - (i + 4)), out var arg1))
-                    {
-                        if (int.TryParse(input.AsSpan(comma + 1, endParen - comma - 1), out var arg2))
-                        {
-                            result += arg1 * arg2;
-                        }
-                    }
-                }
-            }
-        }
-
-        return result.ToString();
+        return ProcessMemory(input, withDonts: false).ToString();
     }
 
     public override string PartTwo(string input)
     {
+        return ProcessMemory(input).ToString();
+    }
+
+    private long ProcessMemory(string memory, bool withDonts = true)
+    {
         var result = 0L;
         var enabled = true;
 
-        for (var i = 0; i < input.Length - 8; i++)
+        for (var i = 0; i < memory.Length - 8; i++)
         {
-            if (input.Substring(i, 4) == "do()")
+            if (withDonts && memory.Substring(i, 4) == "do()")
             {
                 enabled = true;
             }
 
             if (enabled)
             {
-                if (input.Substring(i, 7) == "don't()")
+                if (withDonts && memory.Substring(i, 7) == "don't()")
                 {
                     enabled = false;
                 }
 
-                if (input.Substring(i, 4) == "mul(")
+                if (memory.Substring(i, 4) == "mul(")
                 {
-                    var comma = input.IndexOf(',', i + 4);
-                    var endParen = input.IndexOf(')', comma + 1);
+                    var comma = memory.IndexOf(',', i + 4);
+                    var endParen = memory.IndexOf(')', comma + 1);
 
                     if (comma > 0 && endParen > 0)
                     {
-                        if (int.TryParse(input[(i + 4)..comma], out var arg1))
+                        if (int.TryParse(memory[(i + 4)..comma], out var arg1))
                         {
-                            if (int.TryParse(input[(comma + 1)..endParen], out var arg2))
+                            if (int.TryParse(memory[(comma + 1)..endParen], out var arg2))
                             {
                                 result += arg1 * arg2;
                             }
@@ -70,6 +53,6 @@ public class Day03 : BaseDay
             }
         }
 
-        return result.ToString();
+        return result;
     }
 }

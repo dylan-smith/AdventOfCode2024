2a45573803508edf2dd6857d2f3ba1614451f819
dylan-smith <dylan-smith@github.com>
2024-12-04 14:30:02 -0500

    Day 4.2

--- src/Days/Day04.cs
+++ src/Days/Day04.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Windows.Documents;
+
 namespace AdventOfCode.Days;
 
 [Day(2024, 4)]
@@ -102,6 +104,38 @@ public class Day04 : BaseDay
 
     public override string PartTwo(string input)
     {
-        return string.Empty;
+        var board = input.CreateCharGrid();
+        var result = 0;
+
+        for (var x = 0; x < board.Width() - 2; x++)
+        {
+            for (var y = 0; y < board.Height() - 2; y++)
+            {
+                if (board[x + 1, y + 1] == 'A')
+                {
+                    if (board[x, y] == 'M' && board[x + 2, y] == 'S' && board[x, y + 2] == 'M' && board[x + 2, y + 2] == 'S')
+                    {
+                        result++;
+                    }
+
+                    if (board[x, y] == 'S' && board[x + 2, y] == 'S' && board[x, y + 2] == 'M' && board[x + 2, y + 2] == 'M')
+                    {
+                        result++;
+                    }
+
+                    if (board[x, y] == 'M' && board[x + 2, y] == 'M' && board[x, y + 2] == 'S' && board[x + 2, y + 2] == 'S')
+                    {
+                        result++;
+                    }
+
+                    if (board[x, y] == 'S' && board[x + 2, y] == 'M' && board[x, y + 2] == 'S' && board[x + 2, y + 2] == 'M')
+                    {
+                        result++;
+                    }
+                }
+            }
+        }
+
+        return result.ToString();
     }
 }

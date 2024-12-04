7058412ab72588d24ec603fbd0cbf9de14f41261
dylan-smith <dylan-smith@github.com>
2024-12-04 14:23:23 -0500

    Day 4.1

--- src/AdventConfig.cs
+++ src/AdventConfig.cs
@@ -3,7 +3,7 @@
     public static class AdventConfig
     {
         public const int DefaultYear = 2024;
-        public const int DefaultDay = 3;
+        public const int DefaultDay = 4;
         public const string LogFileFolder = @"C:\git\AdventOfCode2024\logs";
         public const string InputFileFolder = @"C:\git\AdventOfCode2024\input";
         public const string TestFileFolder = @"C:\git\AdventOfCode2024\tests";
--- src/Days/Day04.cs
+++ src/Days/Day04.cs
@@ -1,11 +1,103 @@
-﻿namespace AdventOfCode.Days;
+﻿
+namespace AdventOfCode.Days;
 
 [Day(2024, 4)]
 public class Day04 : BaseDay
 {
     public override string PartOne(string input)
     {
-        return string.Empty;
+        var board = input.CreateCharGrid();
+        var result = 0;
+
+        for (var x = 0; x < board.Width(); x++)
+        {
+            for (var y = 0; y < board.Height() - 3; y++)
+            {
+                if (board[x, y] == 'X' && board[x, y + 1] == 'M' && board[x, y + 2] == 'A' && board[x, y + 3] == 'S')
+                {
+                    result++;
+                }
+            }
+        }
+
+        for (var x = 0; x < board.Width(); x++)
+        {
+            for (var y = board.Height() - 1; y > 2; y--)
+            {
+                if (board[x, y] == 'X' && board[x, y - 1] == 'M' && board[x, y - 2] == 'A' && board[x, y - 3] == 'S')
+                {
+                    result++;
+                }
+            }
+        }
+
+        for (var y = 0; y < board.Height(); y++)
+        {
+            for (var x = 0; x < board.Width() - 3; x++)
+            {
+                if (board[x, y] == 'X' && board[x + 1, y] == 'M' && board[x + 2, y] == 'A' && board[x + 3, y] == 'S')
+                {
+                    result++;
+                }
+            }
+        }
+
+        for (var y = 0; y < board.Height(); y++)
+        {
+            for (var x = board.Width() - 1; x > 2; x--)
+            {
+                if (board[x, y] == 'X' && board[x - 1, y] == 'M' && board[x - 2, y] == 'A' && board[x - 3, y] == 'S')
+                {
+                    result++;
+                }
+            }
+        }
+
+        for (var x = 0; x < board.Width() - 3; x++)
+        {
+            for (var y = 0; y < board.Height() - 3; y++)
+            {
+                if (board[x, y] == 'X' && board[x + 1, y + 1] == 'M' && board[x + 2, y + 2] == 'A' && board[x + 3, y + 3] == 'S')
+                {
+                    result++;
+                }
+            }
+        }
+
+        for (var x = board.Width() - 1; x > 2; x--)
+        {
+            for (var y = board.Height() - 1; y > 2; y--)
+            {
+                if (board[x, y] == 'X' && board[x - 1, y - 1] == 'M' && board[x - 2, y - 2] == 'A' && board[x - 3, y - 3] == 'S')
+                {
+                    result++;
+                }
+            }
+        }
+
+        for (var x = board.Width() - 1; x > 2; x--)
+        {
+            for (var y = 0; y < board.Height() - 3; y++)
+            {
+                if (board[x, y] == 'X' && board[x - 1, y + 1] == 'M' && board[x - 2, y + 2] == 'A' && board[x - 3, y + 3] == 'S')
+                {
+                    result++;
+                }
+            }
+        }
+
+        for (var x = 0; x < board.Width() - 3; x++)
+        {
+            for (var y = board.Height() - 1; y > 2; y--)
+            {
+                if (board[x, y] == 'X' && board[x + 1, y - 1] == 'M' && board[x + 2, y - 2] == 'A' && board[x + 3, y - 3] == 'S')
+                {
+                    result++;
+                }
+            }
+        }
+
+        return result.ToString();
     }
 
     public override string PartTwo(string input)

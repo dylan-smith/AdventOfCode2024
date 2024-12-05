10482040e467f2f82b6311f82bd4b294b7d26a07
dylan-smith <dylan-smith@github.com>
2024-12-05 12:53:07 -0500

    Day 5.2

--- src/Days/Day05.cs
+++ src/Days/Day05.cs
@@ -1,4 +1,8 @@
-﻿namespace AdventOfCode.Days;
+﻿
+
+using System.Diagnostics.Metrics;
+
+namespace AdventOfCode.Days;
 
 [Day(2024, 5)]
 public class Day05 : BaseDay
@@ -49,6 +53,54 @@ public class Day05 : BaseDay
 
     public override string PartTwo(string input)
     {
-        return string.Empty;
+        var rules = input.Paragraphs().First().ParseLines(ParseRule).ToList();
+        var updates = input.Paragraphs().Last().ParseLines(line => line.Integers().ToList()).ToList();
+
+        var result = 0L;
+
+        foreach (var update in updates)
+        {
+            if (!UpdateIsValid(update, rules))
+            {
+                var newUpdate = ReorderUpdate(update, rules);
+
+                result += GetMiddlePage(newUpdate);
+            }
+        }
+
+        return result.ToString();
+    }
+
+    private List<int> ReorderUpdate(List<int> update, List<(int Before, int After)> rules)
+    {
+        var validRules = GetValidRules(rules, update).ToList();
+        var result = new List<int>();
+
+        while (validRules.Count > 0)
+        {
+            foreach (var (Before, After) in validRules)
+            {
+                if (!validRules.Any(x => x.After == Before))
+                {
+                    result.Add(Before);
+                    break;
+                }
+            }
+
+            validRules.RemoveAll(x => result.Contains(x.Before));
+        }
+
+        return result;
+    }
+
+    private IEnumerable<(int Before, int After)> GetValidRules(List<(int Before, int After)> rules, List<int> update)
+    {
+        foreach (var rule in rules)
+        {
+            if (update.Contains(rule.Before) && update.Contains(rule.After))
+            {
+                yield return rule;
+            }
+        }
     }
 }

f5942d0f1284205ef3f352fbb4e9c160cd0aa348
dylan-smith <dylan-smith@github.com>
2024-12-05 12:45:05 -0500

    Day 5.1

--- src/Days/Day05.cs
+++ src/Days/Day05.cs
@@ -5,7 +5,46 @@ public class Day05 : BaseDay
 {
     public override string PartOne(string input)
     {
-        return string.Empty;
+        var rules = input.Paragraphs().First().ParseLines(ParseRule).ToList();
+        var updates = input.Paragraphs().Last().ParseLines(line => line.Integers().ToList()).ToList();
+
+        var result = 0L;
+
+        foreach (var update in updates)
+        {
+            if (UpdateIsValid(update, rules))
+            {
+                result += GetMiddlePage(update);
+            }
+        }
+
+        return result.ToString();
+    }
+
+    private long GetMiddlePage(List<int> update) => update[update.Count / 2];
+
+    private bool UpdateIsValid(List<int> update, List<(int Before, int After)> rules)
+    {
+        foreach (var (Before, After) in rules)
+        {
+            if (update.Contains(Before) && update.Contains(After))
+            {
+                if (update.IndexOf(Before) > update.IndexOf(After))
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    private (int Before, int After) ParseRule(string line)
+    {
+        var before = int.Parse(line.Split('|').First());
+        var after = int.Parse(line.Split("|").Last());
+
+        return (before, after);
     }
 
     public override string PartTwo(string input)

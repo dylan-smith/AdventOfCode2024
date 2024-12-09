2043893467d510f3f324c4c75f2c27e6b667cbd9
dylan-smith <dylan-smith@github.com>
2024-12-09 10:59:29 -0500

    Day 7.1

--- src/AdventConfig.cs
+++ src/AdventConfig.cs
@@ -3,7 +3,7 @@
     public static class AdventConfig
     {
         public const int DefaultYear = 2024;
-        public const int DefaultDay = 6;
+        public const int DefaultDay = 7;
         public const string LogFileFolder = @"C:\git\AdventOfCode2024\logs";
         public const string InputFileFolder = @"C:\git\AdventOfCode2024\input";
         public const string TestFileFolder = @"C:\git\AdventOfCode2024\tests";
--- src/Days/Day07.cs
+++ src/Days/Day07.cs
@@ -1,11 +1,62 @@
-﻿namespace AdventOfCode.Days;
+﻿
+namespace AdventOfCode.Days;
 
 [Day(2024, 7)]
 public class Day07 : BaseDay
 {
     public override string PartOne(string input)
     {
-        return string.Empty;
+        var equations = input.ParseLines(ParseEquation).ToList();
+
+        var good = equations.Where(x => IsEquationTrue(x.Answer, x.Numbers)).ToList();
+
+        foreach (var g in good)
+        {
+            Log($"GOOD EQUATION: {g.Answer}");
+            IsEquationTrue(g.Answer, g.Numbers);
+        }
+
+        return good.Sum(x => x.Answer).ToString();
+    }
+
+    private bool IsEquationTrue(long answer, IEnumerable<long> numbers)
+    {
+        if (numbers.Count() == 1)
+        {
+            return answer == numbers.First();
+        }
+
+        // addition
+        var newNumbers = numbers.Skip(1);
+        var newAnswer = answer - numbers.First();
+
+        if (IsEquationTrue(newAnswer, newNumbers))
+        {
+            Log($"{numbers.First()} + ...");
+            return true;
+        }
+
+        // multiplication
+        newAnswer = answer / numbers.First();
+        var remainder = answer % numbers.First();
+
+        if (remainder == 0 && IsEquationTrue(newAnswer, newNumbers))
+        {
+            Log($"{numbers.First()} * ...");
+            return true;
+        }
+
+        return false;
+    }
+
+    private (long Answer, IEnumerable<long> Numbers) ParseEquation(string line)
+    {
+        var parts = line.Split(new string[] { " ", ":" }, StringSplitOptions.RemoveEmptyEntries);
+
+        var answer = long.Parse(parts.First());
+        var numbers = parts.Skip(1).Select(long.Parse);
+
+        return (answer, numbers.Reverse());
     }
 
     public override string PartTwo(string input)

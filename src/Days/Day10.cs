d766104d1a2b742c8563c9c893fd2e1b71663dd1
dylan-smith <dylan-smith@github.com>
2024-12-10 11:54:52 -0500

    day 10 refactor

--- src/Days/Day10.cs
+++ src/Days/Day10.cs
@@ -11,6 +11,13 @@ public class Day10 : BaseDay
     {
         var map = input.CreateCharGrid();
 
+        var trailheads = FindTrails(map);
+
+        return trailheads.Values.Sum(x => x.Distinct().Count()).ToString();
+    }
+
+    private Dictionary<Point, IEnumerable<Point>> FindTrails(char[,] map)
+    {
         var trailheads = new Dictionary<Point, IEnumerable<Point>>();
 
         foreach (var trailhead in map.GetPoints('0'))
@@ -39,40 +46,14 @@ public class Day10 : BaseDay
             }
         }
 
-        return trailheads.Values.Sum(x => x.Distinct().Count()).ToString();
+        return trailheads;
     }
 
     public override string PartTwo(string input)
     {
         var map = input.CreateCharGrid();
 
-        var trailheads = new Dictionary<Point, IEnumerable<Point>>();
-
-        foreach (var trailhead in map.GetPoints('0'))
-        {
-            trailheads.Add(trailhead, new List<Point>() { trailhead });
-        }
-
-        for (var i = 1; i <= 9; i++)
-        {
-            foreach (var th in trailheads.Keys)
-            {
-                var newTrails = new List<Point>();
-
-                foreach (var trail in trailheads[th])
-                {
-                    foreach (var neighbor in trail.GetNeighbors(includeDiagonals: false))
-                    {
-                        if (map.IsValidPoint(neighbor) && map[neighbor.X, neighbor.Y] == i.ToString()[0])
-                        {
-                            newTrails.Add(neighbor);
-                        }
-                    }
-                }
-
-                trailheads[th] = newTrails;
-            }
-        }
+        var trailheads = FindTrails(map);
 
         return trailheads.Values.Sum(x => x.Count()).ToString();
     }

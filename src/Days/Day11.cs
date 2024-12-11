382cabed301c8c5dfbfe99dd2c08464e355bd031
dylan-smith <dylan-smith@github.com>
2024-12-11 12:58:20 -0500

    Day 11.2

--- src/Days/Day11.cs
+++ src/Days/Day11.cs
@@ -1,4 +1,7 @@
-﻿namespace AdventOfCode.Days;
+﻿
+using System.Xml.Linq;
+
+namespace AdventOfCode.Days;
 
 [Day(2024, 11)]
 public class Day11 : BaseDay
@@ -48,6 +51,61 @@ public class Day11 : BaseDay
 
     public override string PartTwo(string input)
     {
-        return string.Empty;
+        var values = input.Longs().ToList();
+        var result = 0L;
+
+        foreach (var stone in values)
+        {
+            result += ProcessStone(stone, 75);
+        }
+
+        return result.ToString();
+    }
+
+    private readonly Dictionary<long, Dictionary<int, long>> cache = new Dictionary<long, Dictionary<int, long>>();
+
+    private long ProcessStone(long stone, int blinks)
+    {
+        if (blinks == 0)
+        {
+            return 1;
+        }
+
+        if (cache.ContainsKey(stone))
+        {
+            if (cache[stone].ContainsKey(blinks))
+            {
+                return cache[stone][blinks];
+            }
+        }
+        else
+        {
+            cache.Add(stone, new Dictionary<int, long>());
+        }
+
+        long result;
+
+        if (stone == 0)
+        {
+            result = ProcessStone(1, blinks - 1);
+
+            return result; ;
+        }
+        else if (stone.ToString().Length % 2 == 0)
+        {
+            var left = long.Parse(stone.ToString()[..(stone.ToString().Length / 2)]);
+            var right = long.Parse(stone.ToString()[(stone.ToString().Length / 2)..]);
+
+            result = ProcessStone(left, blinks - 1) + ProcessStone(right, blinks - 1);
+
+            return result;
+        }
+        else
+        {
+            result = ProcessStone(stone * 2024, blinks - 1);
+        }
+
+        cache[stone].SafeSet(blinks, result);
+        return result;
     }
 }

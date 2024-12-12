e78c0a63812577936216ec60a2566ee698ee9cc6
Dylan Smith <dylanfromwinnipeg@gmail.com>
2024-12-12 09:03:53 -0600

    wip

--- src/Days/Day12.cs
+++ src/Days/Day12.cs
@@ -1,4 +1,5 @@
 ﻿using System.Drawing;
+using System.Reflection.Metadata;
 
 namespace AdventOfCode.Days;
 
@@ -101,6 +102,116 @@ public class Day12 : BaseDay
 
     public override string PartTwo(string input)
     {
-        return string.Empty;
+        var map = input.CreateCharGrid();
+        var regions = GetRegions(map);
+
+        var result = 0L;
+
+        foreach (var (_, points) in regions)
+        {
+            var sides = CountSides(points);
+            result += points.Count * sides;
+        }
+
+        return result.ToString();
+    }
+
+    private int CountSides(List<Point> points)
+    {
+        var fences = GetFences(points);
+        var used = new List<(Point a, Point b, Direction dir)>();
+        var sides = 0;
+        
+        foreach (var fence in fences)
+        {
+            if (!used.Contains(fence))
+            {
+                sides++;
+
+                used.Add(fence);
+
+                if (fence.dir == Direction.Right)
+                {
+                    var x = fence.a.X;
+
+                    while (fences.Contains((new Point(x + 1, fence.a.Y), new Point(x + 1, fence.b.Y), Direction.Right)))
+                    {
+                        used.Add((new Point(x + 1, fence.a.Y), new Point(x + 1, fence.b.Y), Direction.Right));
+                        x++;
+                    }
+
+                    x = fence.a.X;
+
+                    while (fences.Contains((new Point(x - 1, fence.a.Y), new Point(x - 1, fence.b.Y), Direction.Right)))
+                    {
+                        used.Add((new Point(x - 1, fence.a.Y), new Point(x - 1, fence.b.Y), Direction.Right));
+                        x--;
+                    }
+                }
+                else
+                {
+                    var y = fence.a.Y;
+
+                    while (fences.Contains((new Point(fence.a.X, y + 1), new Point(fence.b.X, y + 1), Direction.Up)))
+                    {
+                        used.Add((new Point(fence.a.X, y + 1), new Point(fence.b.X, y + 1), Direction.Up));
+                        y++;
+                    }
+
+                    y = fence.a.Y;
+
+                    while (fences.Contains((new Point(fence.a.X, y - 1), new Point(fence.b.X, y - 1), Direction.Up)))
+                    {
+                        used.Add((new Point(fence.a.X, y - 1), new Point(fence.b.X, y - 1), Direction.Up));
+                        y--;
+                    }
+                }
+
+
+            }
+        }
+
+        return sides;
+    }
+
+    private List<(Point a, Point b, Direction dir)> GetFences(List<Point> points)
+    {
+        var fences = new List<(Point a, Point b, Direction dir)>();
+
+        foreach (var p in points)
+        {
+            var neighbors = p.GetNeighbors(includeDiagonals: false);
+
+            foreach (var n in neighbors)
+            {
+                if (!points.Contains(n))
+                {
+                    if (p.X == n.X)
+                    {
+                        if (p.Y < n.Y)
+                        {
+                            fences.Add((p, n, Direction.Right));
+                        }
+                        else
+                        {
+                            fences.Add((n, p, Direction.Right));
+                        }
+                    }
+                    else
+                    {
+                        if (p.X < n.X)
+                        {
+                            fences.Add((p, n, Direction.Up));
+                        }
+                        else
+                        {
+                            fences.Add((n, p, Direction.Up));
+                        }
+                    }
+                }
+            }
+        }
+
+        return fences;
     }
 }
